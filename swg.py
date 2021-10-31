153114300f4bf99828c1facba773f228ea9ea598
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2021-10-31 21:03:23 +0530

    Update swg.py

--- swg.py
+++ swg.py
@@ -11,4 +11,37 @@ while True:
       print("pc:",pc)
       print("player:",player)
       print("It's tie")
+      elif player=="snake":
+        if pc=="water":
+          print("pc:",pc)
+          print("player:",player)
+          print("you win")
+        if pc=="gun":
+          print("pc:",pc)
+          print("player:",player)
+          print("you lose")
+       elif player=="water":
+        if pc=="gun":
+          print("pc:",pc)
+          print("player:",player)
+          print("you lose")
+        if pc=="snake":
+          print("pc:",pc)
+          print("player:",player)
+          print("you win")
+        elif player=="gun":
+          if pc=="snake":
+          print("pc:",pc)
+          print("player:",player)
+          print("you win")
+          if pc=="water":
+          print("pc:",pc)
+          print("player:",player)
+          print("you lose")
+play_again=input("Do you want to play again?(Yes/No):").lower()
+if play_again!="yes":
+  break
+  print("Game Over")
+          
+          
       
