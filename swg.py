bc75d94a4852a907e95f3f73c715f24c7d61cc69
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2021-10-31 19:37:41 +0530

    Update swg.py

--- swg.py
+++ swg.py
@@ -1,2 +1,14 @@
 # Snake_Water_Gun
-A game using python
+#A game using python
+import random
+while True:
+  choices=["snake","water","gun"]
+  pc=random.choice(choices)
+  player=None
+  while player not in choices:
+    player=input("snake,water,or gun?").lower()
+    if player==pc:
+      print("pc:",pc)
+      print("player:",player)
+      print("It's tie")
+      
